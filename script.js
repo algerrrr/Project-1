3f0af20bd65de3c5b3745477d2ec0fd106a40b74
Algernon Soriano <alger.soriano@gmail.com>
2023-09-17 02:33:30 +1000

    adding functions

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.getElementById('search-form').addEventListener('submit', function (e) {
 async function searchMovies(query) {
     // Use your TMDb API key here
     const apiKey = '726246bb7ec5fbb1a80d0e538bfee10a';
-
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
     try {
@@ -23,8 +22,10 @@ async function searchMovies(query) {
         movieResults.innerHTML = '';
 
         if (data.results && data.results.length > 0) {
+            
             data.results.forEach(movie => {
                 const movieElement = document.createElement('div');
+                
                 movieElement.classList.add('movie');
                 movieElement.innerHTML = `
                     
@@ -42,3 +43,37 @@ async function searchMovies(query) {
         console.error('Error fetching data:', error);
     }
 }
+async function showMovies() {
+    
+}
+function getPopularMovies() {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjYyNDZiYjdlYzVmYmIxYTgwZDBlNTM4YmZlZTEwYSIsInN1YiI6IjY1MDVhMGYwZmEyN2Y0MDEwYzQ5ZDg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thKokVSgsNkuUE5lHe4mmcATo5apThkx08FlvnEZ6NU'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+getPopularMovies()
+
+function getPopularSeries() {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjYyNDZiYjdlYzVmYmIxYTgwZDBlNTM4YmZlZTEwYSIsInN1YiI6IjY1MDVhMGYwZmEyN2Y0MDEwYzQ5ZDg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thKokVSgsNkuUE5lHe4mmcATo5apThkx08FlvnEZ6NU'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+getPopularSeries()
