47f8fe265c3507112b9209ef466dba8a4d0a58f6
Algernon Soriano <alger.soriano@gmail.com>
2023-09-17 00:05:58 +1000

    adding api

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-// Get references to the form, input, and movie results container
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
-const movieResults = document.getElementById('movie-results');
+// script.js
 
-// Add a submit event listener to the form
-searchForm.addEventListener('submit', async function (e) {
-    e.preventDefault(); // Prevent the default form submission behavior
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
 
-    // Get the user's search query from the input field
-    const query = searchInput.value;
+    const query = document.getElementById('search-input').value;
 
-    // Clear previous search results
-    movieResults.innerHTML = '';
-
-    // Perform the movie search using the TMDb API
-    const results = await searchMovies(query);
-
-    // Display the search results
-    if (results && results.length > 0) {
-        results.forEach(movie => {
-            const movieElement = document.createElement('div');
-            movieElement.classList.add('movie');
-            movieElement.innerHTML = `
-                <h2>${movie.title}</h2>
-                <p>Release Date: ${movie.release_date}</p>
-                <p>Overview: ${movie.overview}</p>
-            `;
-            movieResults.appendChild(movieElement);
-        });
-    } else {
-        movieResults.innerHTML = 'No results found.';
-    }
+    // Call a function to fetch and display movie search results from the TMDb API
+    searchMovies(query);
 });
+
+async function searchMovies(query) {
+    // Use your TMDb API key here
+    const apiKey = '726246bb7ec5fbb1a80d0e538bfee10a';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const movieResults = document.getElementById('movie-results');
+        movieResults.innerHTML = '';
+
+        if (data.results && data.results.length > 0) {
+            data.results.forEach(movie => {
+                const movieElement = document.createElement('div');
+                movieElement.classList.add('movie');
+                movieElement.innerHTML = `
+                    
+                    <img src="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="${movie.title} Poster">
+                    <h2>${movie.title}</h2>
+                    <p>Release Date: ${movie.release_date}</p>
+                    
+                `;
+                movieResults.appendChild(movieElement);
+            });
+        } else {
+            movieResults.innerHTML = 'No results found.';
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
