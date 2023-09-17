0907eeb856be3ccc3f6c0a65836890fac64d9195
Algernon Soriano <alger.soriano@gmail.com>
2023-09-17 16:37:43 +1000

    add event listener

--- script.js
+++ script.js
@@ -1,197 +1,268 @@
-// script.js
+const main = document.getElementById("movie-results");
+
 const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjYyNDZiYjdlYzVmYmIxYTgwZDBlNTM4YmZlZTEwYSIsInN1YiI6IjY1MDVhMGYwZmEyN2Y0MDEwYzQ5ZDg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thKokVSgsNkuUE5lHe4mmcATo5apThkx08FlvnEZ6NU'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjYyNDZiYjdlYzVmYmIxYTgwZDBlNTM4YmZlZTEwYSIsInN1YiI6IjY1MDVhMGYwZmEyN2Y0MDEwYzQ5ZDg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thKokVSgsNkuUE5lHe4mmcATo5apThkx08FlvnEZ6NU",
+  },
+};
 
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission
+document.getElementById("search-form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the default form submission
 
-    const query = document.getElementById('search-input').value;
+  const query = document.getElementById("search-input").value;
 
-    // Call a function to fetch and display movie search results from the TMDb API
-    searchMovies(query);
+  // Call a function to fetch and display movie search results from the TMDb API
+  searchMovies(query);
 });
 
 async function searchMovies(query) {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
-    const data = await response.json();
-    showElement(data.results)
-    console.log('results',data.results)
-
-
+    main.innerHTML = ""
+  const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,options);
+  const data = await response.json();
+  showElement(data.results);
 }
+document.getElementById('genre').addEventListener('change', function (e) {
+    const genre = document.getElementById('genre')
+    console.log(genre.value)
+    fetchByGenre(genre.value)
+})
+
+async function fetchByGenre(genre) {
+    main.innerHTML = ""
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.asc&with_genres=${genre}`, options)
+  const data = await response.json();
+  showElement(data.results);
+}
+
 function showElement(results) {
-    const main = document.getElementById('movie-results');
-    const div = document.createElement('div');
-    div.classList.add('content')
-    results.forEach(({ title, poster_path }) => {
-        const apiKey = '726246bb7ec5fbb1a80d0e538bfee10a'
-        const li = document.createElement('li');
- 
-        li.classList.add('movie');
-        const link = document.createElement('a');
-        // link.href = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${title}`;
+  const div = document.createElement("div");
+  div.classList.add("content");
+  results.forEach(({ title, poster_path }) => {
+    const apiKey = "726246bb7ec5fbb1a80d0e538bfee10a";
+    const li = document.createElement("li");
 
-        
-        const image = document.createElement('img');
-        image.src = `https://image.tmdb.org/t/p/w185/${poster_path}`;
-        
-        // Create a text node for the movie title
-        const titleText = document.createElement('p');
-        titleText.innerText = title
-        link.append(image);
-        link.append(titleText);
-        li.append(link);
-        div.append(li);
+    li.classList.add("movie");
+    const link = document.createElement("a");
+    const image = document.createElement("img");
+    image.src = `https://image.tmdb.org/t/p/w185/${poster_path}`;
+
+    // Create a text node for the movie title
+    const titleText = document.createElement("p");
+    titleText.innerText = title;
+    link.append(image);
+    link.append(titleText);
+    li.append(link);
+    div.append(li);
+  });
+
+  //
+  div.addEventListener("click", function (e) {
+    let chosenItemFilter = results.filter((element) => {
+      return (
+        "https://image.tmdb.org/t/p/w185/" + element.poster_path ===
+        e.target.src
+      );
     });
+    getMovieDetails(chosenItemFilter[0].id);
+  });
+  //
+  
+  // Append the unordered list to the main element
+  main.append(div);
+  
+}
 
-    // 
-    div.addEventListener('click',function(e){
-        console.log(e.target.src)
-        let outerModal = document.createElement('div')
-        outerModal.classList.add('outer-modal')
-        let detailsDiv = document.createElement('div')
-        detailsDiv.classList.add('movie-details-div')
-        let span = document.createElement('span')
-        span.innerHTML = '&times;'
-        span.addEventListener('click',function(){
-            outerModal.style.display = 'none'
-        })
-        let movieInfoDiv = document.createElement('div')
-        movieInfoDiv.classList.add('movie-info-div')
-       
-        let chosenItem = results.filter((element)=>{
-            return 'https://image.tmdb.org/t/p/w185/'+ element.poster_path === e.target.src
-        })
-        console.log(chosenItem)
 
-        // create elements inside the details div
-        let detailsImg = document.createElement('img')
-        detailsImg.src = 'https://image.tmdb.org/t/p/w185/'+chosenItem[0].poster_path
+function getPopularMovies() {
+  fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+    options
+  )
+    .then((response) => response.json())
+    .then(({ results }) => {
+      const main = document.getElementById("movie-results");
+      const ul = document.createElement("ul");
 
-        let h3 = document.createElement('h3')
-        let p = document.createElement('p')
+      results.forEach(({ title }) => {
+        const li = document.createElement("li");
 
-        h3.innerText = 'Overview'
-        p.innerText = chosenItem[0].overview
-        movieInfoDiv.append(span,h3,p)
-        
-        // add to the detials div
-        
-        detailsDiv.append(detailsImg,movieInfoDiv)
-        outerModal.append(detailsDiv)
-        main.append(outerModal)
+        li.innerText = title;
+        ul.append(li);
+      });
+      main.append(ul);
     })
-    // 
 
-
-    // Append the unordered list to the main element
-    main.append(div);
-}
-
-async function renderPoster() {
-
-
-}
-
-// async function searchMovies(query) {
-//     fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
-//         .then(response => response.json())
-//         .then(({results})=>{
-            
-//             const main = document.getElementById('movie-results')
-//             const ul = document.createElement('ul')
-
-//             results.forEach(({title, poster_path})=>{
-//                 const apiKey = '726246bb7ec5fbb1a80d0e538bfee10a'
-//                 const li = document.createElement('li')
-//                 const link = document.createElement('a')
-//                 const image = document.createElement('img')
-//                 image.href = `https://image.tmdb.org/t/p/w185/${poster_path}`
-//                 link.href = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${title}`;
-//                 link.innerText = title
-                
-//                 li.append(link)
-//                 ul.append(li)
-
-//             })
-//             main.append(ul)
-
-//         })
-
-//         .catch(err => console.error(err));
-// }
-// async function searchMovies(query) {
-//     // Use your TMDb API key here
-//     const apiKey = '726246bb7ec5fbb1a80d0e538bfee10a';
-//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
-
-//     try {
-//         const response = await fetch(url);
-//         const data = await response.json();
-
-//         const movieResults = document.getElementById('movie-results');
-//         movieResults.innerHTML = '';
-
-//         if (data.results && data.results.length > 0) {
-//             data.results.forEach(movie => {
-//                 const movieElement = document.createElement('div');
-                
-//                 movieElement.classList.add('movie');
-//                 movieElement.innerHTML = `
-                    
-//                     <img src="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="${movie.title} Poster">
-//                     <h2>${movie.title}</h2>
-//                     <p>Release Date: ${movie.release_date}</p>
-                    
-//                 `;
-//                 movieResults.append(movieElement);
-//             });
-//         } else {
-//             movieResults.innerHTML = 'No results found.';
-//         }
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//     }
-// }
-//document.getElementById('popular-movies').addEventListener('click', getPopularMovies);
-
-
-function getPopularMovies() {      
-      fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
-        .then(response => response.json())
-        .then(({results})=>{
-            const main = document.getElementById('movie-results')
-            const ul = document.createElement('ul')
-
-            results.forEach(({title})=>{
-                const li = document.createElement('li')
-
-                li.innerText = title
-                ul.append(li)
-
-            })
-            main.append(ul)
-
-        })
-        
-        .catch(err => console.error(err));
-        
+    .catch((err) => console.error(err));
 }
 
 //document.getElementById('popular-series').addEventListener('click', getPopularSeries);
 function getPopularSeries() {
-    
-      
-      fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+  fetch("https://api.themoviedb.org/3/trending/tv/day?language=en-US", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
+
+async function getMovieDetails(id) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjYyNDZiYjdlYzVmYmIxYTgwZDBlNTM4YmZlZTEwYSIsInN1YiI6IjY1MDVhMGYwZmEyN2Y0MDEwYzQ5ZDg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thKokVSgsNkuUE5lHe4mmcATo5apThkx08FlvnEZ6NU",
+    },
+  };
+
+  let response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+    options
+  );
+  // .then(response => response.json())
+  // .then(data => {
+  //     return data
+  // })
+  // .catch(err => console.error(err));
+  let chosenItem = await response.json();
+  console.log(chosenItem);
+
+  let outerModal = document.createElement("div");
+  outerModal.classList.add("outer-modal");
+  let detailsDiv = document.createElement("div");
+  detailsDiv.classList.add("movie-details-div");
+  let closebtnP = document.createElement("p");
+  closebtnP.classList.add("closebtnP");
+  let closeBtn = document.createElement("span");
+  closeBtn.classList.add("close-btn");
+  closeBtn.innerHTML = "&times;";
+  closeBtn.addEventListener("click", function () {
+    outerModal.style.display = "none";
+  });
+  closebtnP.append(closeBtn);
+  let movieInfoDiv = document.createElement("div");
+  movieInfoDiv.classList.add("movie-info-div");
+
+  // create elements inside the details div'
+  let detailsImg = document.createElement("img");
+  detailsImg.src = "https://image.tmdb.org/t/p/w185/" + chosenItem.poster_path;
+
+  // create title
+  const title = document.createElement("h2");
+  title.innerText = chosenItem.original_title;
+
+  // create tagline
+  const taglineDiv = document.createElement("div");
+  const taglineH3 = document.createElement("h3");
+  const taglineP = document.createElement("p");
+  taglineH3.innerText = "TAGLINE";
+  taglineP.innerText = chosenItem.tagline;
+  taglineDiv.append(taglineH3, taglineP);
+
+  // create overview
+  const overviewDiv = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  h3.innerText = "OVERVIEW:";
+  p.innerText = chosenItem.overview;
+  overviewDiv.append(h3, p);
+
+//   create runtime
+const runtimeDiv = document.createElement("div");
+const runtimeh3 = document.createElement("h3");
+const runtimep = document.createElement("p");
+runtimeh3.innerText = "RUNTIME:";
+runtimep.innerText = chosenItem.runtime + ' minutes';
+runtimeDiv.append(runtimeh3, runtimep);
+
+  //create original language
+  const originalLanguageDiv = document.createElement("div");
+  const originalLanguageTitle = document.createElement("h3");
+  const originalLanguage = document.createElement("p");
+  originalLanguageTitle.innerText = "ORIGINAL LANGUAGE:";
+  originalLanguage.innerText = chosenItem.original_language;
+  originalLanguageDiv.append(originalLanguageTitle, originalLanguage);
+
+  // create release date
+  const releaseDateDiv = document.createElement("div");
+  const releaseDateTitle = document.createElement("h3");
+  releaseDateTitle.innerText = "RELEASE DATE:";
+  const releaseDate = document.createElement("p");
+  releaseDate.innerText = chosenItem.release_date;
+  releaseDateDiv.append(releaseDateTitle, releaseDate);
+
+  // create homepage
+  const homepageDiv = document.createElement("div");
+  const homepageTitle = document.createElement("h3");
+  homepageTitle.innerText = "HOMEPAGE: ";
+  const homepageA = document.createElement("a");
+  homepageA.href = chosenItem.homepage ? chosenItem.homepage : '#';
+  homepageA.target = "_blank"
+  homepageA.innerText = chosenItem.homepage ?  chosenItem.homepage : "Sorry, no homepage available.."
+  homepageDiv.append(homepageTitle, homepageA);
+
+  // add to movie info div
+  movieInfoDiv.append(
+    closebtnP,
+    title,
+    taglineDiv,
+    overviewDiv,
+    runtimeDiv,
+    originalLanguageDiv,
+    releaseDateDiv,
+    homepageDiv
+  );
+
+  // add to the detials div
+
+  detailsDiv.append(detailsImg, movieInfoDiv);
+  outerModal.append(detailsDiv);
+  main.append(outerModal);
 }
 
 
+function searchByGenre(results) {
+    const genreSelect = document.getElementById('genre');
+    const genres = {
+      "genres": [
+        {
+          "id": 28,
+          "name": "Action"
+        },
+  
+        {
+          "id": 35,
+          "name": "Comedy"
+        },
+  
+        {
+          "id": 18,
+          "name": "Drama"
+        },
+  
+        {
+          "id": 878,
+          "name": "Science Fiction"
+        },
+      ]
+    };
+  
+    genreSelect.addEventListener('change', function (e) {
+      const genreValue = e.target.value;
+      console.log(genreValue);
+   
+      let filteredResults = results.filter(element => {
+        const genre = genres.genres.find(g => g.name === genreValue);
+        return genre && element.genre_ids.includes(genre.id);
+      });
+      main.innerHTML=""
+   
+      showElement(filteredResults);
+    });
+  }
 
+
+  
+
+  
